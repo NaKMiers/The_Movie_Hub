f16a934b1a4212f96f7502fd450a7263d3a9bc1d
Pi Pi <56575187+NaKMiers@users.noreply.github.com>
2024-05-10 19:58:53 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace the_movie_hub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
